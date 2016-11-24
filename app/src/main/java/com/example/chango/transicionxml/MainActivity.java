e20c6d2ab92a7e744af70703496cea7099c32fe5
alumnoIdat <leomanarael@gmail.com>
2016-11-24 12:16:15 -0500

    Initial commit

---
+++ app/src/main/java/com/example/chango/transicionxml/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.chango.transicionxml;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
